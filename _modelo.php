2088c87643bb1b68151a2a03541e3c52b2cc6973
DanielSLombardo <danielombardo2002@gmail.com>
2022-04-11 13:14:12 -0300

    Aula 3

---
+++ _modelo.php
@@ -0,0 +1,33 @@
+<?php
+// Inclui o cabeçalho da página 
+require('_header.php');
+?>
+            <section>
+                <h2>Título</h2>
+                <p>Quisque vulputate aliquet lectus, quis consectetur leo posuere id. Aliquam finibus justo ac pellentesque vehicula. 
+                    Vestibulum nec lacus purus. Maecenas cursus, sem eget sollicitudin auctor, lorem orci laoreet purus, 
+                    a fermentum massa dolor vel nulla. Suspendisse ac pharetra sem, et suscipit dolor. Phasellus quis lacinia felis. 
+                    Fusce pharetra blandit magna, et auctor tellus pellentesque nec. Nulla consequat neque magna, 
+                    quis mattis tellus hendrerit tempor. Quisque posuere elit velit, quis fermentum nulla scelerisque ut. 
+                    Donec ut egestas tellus. Maecenas urna sem, scelerisque sit amet quam ac, molestie dignissim leo. 
+                    Cras euismod magna metus, in ultrices arcu rhoncus nec. Quisque porta velit sit amet dui varius, 
+                    at volutpat eros interdum. Praesent quis magna viverra, molestie lectus ac, porta lectus.</p>
+                <p>Quisque vulputate aliquet lectus, quis consectetur leo posuere id. Aliquam finibus justo ac pellentesque vehicula. 
+                    Vestibulum nec lacus purus. Maecenas cursus, sem eget sollicitudin auctor, lorem orci laoreet purus, 
+                    a fermentum massa dolor vel nulla. Suspendisse ac pharetra sem, et suscipit dolor. Phasellus quis lacinia felis. 
+                    Fusce pharetra blandit magna, et auctor tellus pellentesque nec. Nulla consequat neque magna, 
+                    quis mattis tellus hendrerit tempor. Quisque posuere elit velit, quis fermentum nulla scelerisque ut. 
+                    Donec ut egestas tellus. Maecenas urna sem, scelerisque sit amet quam ac, molestie dignissim leo. 
+                    Cras euismod magna metus, in ultrices arcu rhoncus nec. Quisque porta velit sit amet dui varius, 
+                    at volutpat eros interdum. Praesent quis magna viverra, molestie lectus ac, porta lectus.</p>
+            </section>
+
+            <aside>
+                <h3>Lateral</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, aperiam corporis culpa consequatur
+                    iusto.</p>
+            </aside>
+<?php
+// Inclui o rodapé
+require('_footer.php');
+?>
